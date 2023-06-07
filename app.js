fb2f3622b925a80543dc9daf74687e3a96128f30
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-07 16:37:45 +0330

    change img src

--- app.js
+++ app.js
@@ -9,77 +9,77 @@ const certificatesDiv = document.querySelector("#certs");
 const certificates = [
   {
     certName: "HTML and CSS in depth",
-    imgSrc: "./images/certificates/HTML_CSS.jpg",
+    imgSrc: "HTML_CSS.jpg",
     alt: "HTML & CSS Certificates",
     organization: "Coursera",
     certDate: "Dec 18, 2022",
   },
   {
     certName: "Version Control",
-    imgSrc: "./images/certificates/version_control.jpg",
+    imgSrc: "version_control.jpg",
     alt: "Version Control Certificate",
     organization: "Coursera",
     certDate: "Nov 15, 2022",
   },
   {
     certName: "Programming with Javascript",
-    imgSrc: "./images/certificates/JavaScript_Meta.jpg",
+    imgSrc: "JavaScript_Meta.jpg",
     alt: "Programming with Javascript",
     organization: "Coursera",
     certDate: "Nov 20, 2022",
   },
   {
     certName: "Introduction to Front-End Development",
-    imgSrc: "./images/certificates/Intro_to_frontend.jpg",
+    imgSrc: "Intro_to_frontend.jpg",
     alt: "Introduction to Front-End Development",
     organization: "Coursera",
     certDate: "Oct 28, 2022",
   },
   {
     certName: "Introduction to Database for Back-End Development",
-    imgSrc: "./images/certificates/Intro_to_DB_for_Backend.jpg",
+    imgSrc: "Intro_to_DB_for_Backend.jpg",
     alt: "Introduction to Database for Back-End Development",
     organization: "Coursera",
     certDate: "Dec 10, 2022",
   },
   {
     certName: "Introduction to Database",
-    imgSrc: "./images/certificates/Intro_to_DB.jpg",
+    imgSrc: "Intro_to_DB.jpg",
     alt: "Introduction to Database",
     organization: "Coursera",
     certDate: "Dec 10, 2022",
   },
   {
     certName: "Programming in Python",
-    imgSrc: "./images/certificates/programming_in_python.jpg",
+    imgSrc: "programming_in_python.jpg",
     alt: "Programming in Python",
     organization: "Coursera",
     certDate: "Dec 04, 2022",
   },
   {
     certName: "Introduction to Back-End Development",
-    imgSrc: "./images/certificates/intro_to_back-end.jpg",
+    imgSrc: "intro_to_back-end.jpg",
     alt: "Introduction to Back-End Development",
     organization: "Coursera",
     certDate: "Nov 17, 2022",
   },
   {
     certName: "Responsive Web Design",
-    imgSrc: "./images/certificates/ResponsiveWeb.png",
+    imgSrc: "ResponsiveWeb.png",
     alt: "Responsive Web Design",
     organization: "freeCodeCamp",
     certDate: "Jun 4, 2022",
   },
   {
     certName: "Introduction to Machine Learning",
-    imgSrc: "./images/certificates/Kaggle-Intro-to-Machine-Learning.png",
+    imgSrc: "Kaggle-Intro-to-Machine-Learning.png",
     alt: "Introduction to Machine Learning",
     organization: "Kaggle",
     certDate: "Dec 1, 2020",
   },
   {
     certName: "Python",
-    imgSrc: "./images/certificates/Kaggle-Python.png",
+    imgSrc: "Kaggle-Python.png",
     alt: "Python",
     organization: "Kaggle",
     certDate: "March 8, 2021",
@@ -87,7 +87,7 @@ const certificates = [
   {
     certName:
       "Microsoft Certified Solution Associate : <span> Web Applications Charter Member</span>",
-    imgSrc: "./images/certificates/MCSD.jpg",
+    imgSrc: "MCSD.jpg",
     alt: " Microsoft Certified Solution Associate : Web Applications Charter Member",
     organization: "Microsoft",
     certDate: "Sep 26, 2016",
@@ -95,7 +95,7 @@ const certificates = [
   {
     certName:
       "Microsoft Certified Solution Developer : <span>Web Applications</span>",
-    imgSrc: "./images/certificates/Kaggle-Python.png",
+    imgSrc: "Kaggle-Python.png",
     alt: "Microsoft Certified Solution Developer : Web Applications",
     organization: "Kaggle",
     certDate: " Jun 29, 2013",
@@ -103,7 +103,7 @@ const certificates = [
 
   {
     certName: "Microsoft Certified Professional",
-    imgSrc: "./images/certificates/MCP.jpg",
+    imgSrc: "MCP.jpg",
     alt: "Microsoft Certified Professional",
     organization: "Microsoft",
     certDate: "Apr 3, 2013",
@@ -111,7 +111,7 @@ const certificates = [
   {
     certName:
       "Microsoft Specialist :<span> Programming in HTML5 with Javascript and CSS3</span>",
-    imgSrc: "./images/certificates/MicrosoftSpecialist.jpg",
+    imgSrc: "MicrosoftSpecialist.jpg",
     alt: "Microsoft Specialist : Programming in HTML5 with Javascript and CSS3",
     organization: "Microsoft",
     certDate: "Apr 3, 2016",
@@ -165,7 +165,7 @@ const addCertificates = () => {
     const certDiv = document.createElement("div");
     certDiv.classList.add("certificate");
     const newCert = `
-                <img src="${cert.imgSrc}" alt="${cert.alt}">
+                <img src="./images/certificates/${cert.imgSrc}" alt="${cert.alt}">
                 <div class="certificate-text">
                     <h4>${cert.organization}</h4>
                     <p class="date">${cert.certDate}</p>
